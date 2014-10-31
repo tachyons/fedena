cbaecab81a3db781ed28e15a65fd6101ae68c8a7
Aboobacker MK <aboobackervyd@gmail.com>
2014-10-31 16:31:24 +0530

    upgaraded to 2.3.14

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,3 +11,16 @@ document.observe("dom:loaded", function() {
 
     load_menu_from_plugins();
 });
+Ajax.Responders.register({
+  onCreate: function(request) {
+    var csrf_meta_tag = $$('meta[name=csrf-token]')[0];
+    if (csrf_meta_tag) {
+      var header = 'X-CSRF-Token',
+          token = csrf_meta_tag.readAttribute('content');
+      if (!request.options.requestHeaders) {
+        request.options.requestHeaders = {};
+      }
+      request.options.requestHeaders[header] = token;
+    }
+  }
+});
--- public/javascripts/fedena_plugin.js
+++ public/javascripts/fedena_plugin.js
@@ -1,16 +1,4 @@
-Ajax.Responders.register({
-  onCreate: function(request) {
-    var csrf_meta_tag = $$('meta[name=csrf-token]')[0];
-    if (csrf_meta_tag) {
-      var header = 'X-CSRF-Token',
-          token = csrf_meta_tag.readAttribute('content');
-      if (!request.options.requestHeaders) {
-        request.options.requestHeaders = {};
-      }
-      request.options.requestHeaders[header] = token;
-    }
-  }
-});
+
 function render_generic_hook(hooks){
 
     var link_box, link_head, link_desc, link, target;
